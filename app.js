61865c32d2547fac183417729d15c9677c8a9643
Rotich2000 <drkimutai06@gmail.com>
2022-03-21 19:45:44 +0300

    My landing page website

---
+++ app.js
@@ -0,0 +1,53 @@
+// Select the elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+
+declare = () =>{
+     toggle_btn = document.querySelector(".toggle-btn");
+     big_wrapper = document.querySelector('.big-wrapper');  
+     hamburger_menu = document.querySelector('.hamburger-menu');
+}
+
+const main = document.querySelector('main');
+declare()
+
+let dark = false
+
+toggleAnimation=() => {
+    // Clone the wrapper
+    dark=!dark;
+    let clone = big_wrapper.cloneNode(true);
+    if(dark){
+        clone.classList.remove("light")
+        clone.classList.add("dark")
+    }else{
+        clone.classList.remove("dark")
+        clone.classList.add("light")
+    }
+    clone.classList.add("copy")
+    main.appendChild(clone);
+
+    clone.addEventListener("animationend", () => {
+        big_wrapper.remove()
+        clone.classList.remove("copy")
+        // Reset variables
+        declare();
+        events()
+    } )
+
+    }
+
+    events = () => {
+        toggle_btn.addEventListener("click",toggleAnimation);
+        hamburger_menu.addEventListener("click",() => {
+            big_wrapper.classList.toggle("active")
+        })
+    }
+
+    events()
+
+
+   
+
